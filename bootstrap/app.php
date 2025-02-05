9e5e5a11fe0aabdf3ad10a2ae5f1fa155a16d8c5
Nirav1024 <nickthanki74@gmail.com>
2025-02-05 11:33:16 +0530

    404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use \Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (HttpException $exception, Request $request) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view("404", [], 404);
+            }
+        });
     })->create();
